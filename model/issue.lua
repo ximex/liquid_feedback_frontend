23c9f0316258e3265c1f29961bac8915421bfc79
bsw <bsw@localhost>
2013-08-11 23:00:55 +0200

    Fixed syntax error from issue model

--- model/issue.lua
+++ model/issue.lua
@@ -197,7 +197,7 @@ function Issue:get_state_name_for_state(value)
     canceled_issue_not_accepted = _"Canceled (issue not accepted)",
     canceled_after_revocation_during_discussion = _"Canceled (during discussion due to revocation)",
     canceled_after_revocation_during_verification = _"Canceled (during verification due to revocation)",
-    canceled_by_admin = _"Canceled by administrative intervention"
+    canceled_by_admin = _"Canceled by administrative intervention",
     calculation = _"Calculation",
     canceled_no_initiative_admitted = _"Canceled (no initiative admitted)",
     finished_without_winner = _"Finished (without winner)",
