c45a8532432c2284ce7a933affcde3de5295278b
bsw <bsw@localhost>
2013-08-11 22:23:35 +0200

    Added action for admin cancel issue

---
+++ app/main/admin/_action/cancel_issue.lua
@@ -0,0 +1,27 @@
+local issue = Issue
+  :new_selector()
+  :add_where{ "id = ?", param.get_id()}
+  :single_object_mode()
+  :for_update()
+  :exec()
+
+if issue.closed then
+  error("issue is already closed")
+  return false
+end  
+
+issue.state = "canceled_by_admin"
+issue.closed = "now"
+
+local admin_notice
+if issue.admin_notice then
+  admin_notice = issue.admin_notice .. "\n\n"
+else
+  admin_notice = ""
+end
+
+admin_notice = admin_notice .. param.get("admin_notice")
+
+issue.admin_notice = admin_notice
+
+issue:save()
