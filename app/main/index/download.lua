7a48a37feb806bc97bfa2359658fb85ebc8e1877
bsw <bsw@localhost>
2014-07-16 21:47:34 +0200

    Removed not needed check

---
+++ app/main/index/download.lua
@@ -0,0 +1,58 @@
+if not config.download_dir then
+  error("feature not enabled")
+end
+
+slot.put_into("title", _"Download database export")
+
+slot.select("actions", function()
+  ui.link{
+    content = function()
+        ui.image{ static = "icons/16/cancel.png" }
+        slot.put(_"Cancel")
+    end,
+    module = "index",
+    view = "index"
+  }
+end)
+
+ui.container{
+  attr = { class = "wiki use_terms" },
+  content = function()
+    slot.put(config.download_use_terms)
+  end
+}
+
+
+local file_list = extos.listdir(config.download_dir)
+
+local tmp = {}
+for i, filename in ipairs(file_list) do
+  if not filename:find("^%.") then
+    tmp[#tmp+1] = filename
+  end
+end
+
+local file_list = tmp
+
+table.sort(file_list, function(a, b) return a > b end)
+
+ui.list{
+  records = file_list,
+  columns = {
+    {
+      content = function(filename)
+        slot.put(encode.html(filename))
+      end
+    },
+    {
+      content = function(filename)
+        ui.link{
+          content = _"Download",
+          module = "index",
+          view = "download_file",
+          params = { filename = filename }
+        }
+      end
+    }
+  }
+}
