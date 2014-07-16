1ef2e99d87ccff15efef7fefaf7da8e74edde655
bsw <bsw@localhost>
2014-07-16 21:52:17 +0200

    New layout for suggestion details view

--- app/main/suggestion/show.lua
+++ app/main/suggestion/show.lua
@@ -17,29 +17,112 @@ if not suggestion then
   return
 end
 
+local initiative = suggestion.initiative
 
-app.html_title.title = suggestion.name
-app.html_title.subtitle = _("Suggestion ##{id}", { id = suggestion.id })
+initiative:load_everything_for_member_id(app.session.member_id)
+initiative.issue:load_everything_for_member_id(app.session.member_id)
 
-ui.title(_"Suggestion for initiative: '#{name}'":gsub("#{name}", suggestion.initiative.name))
 
-ui.actions(function()
-  ui.link{
-    content = function()
-        ui.image{ static = "icons/16/resultset_previous.png" }
-        slot.put(_"Back")
-    end,
-    module = "initiative",
-    view = "show",
-    id = suggestion.initiative.id,
-    params = { tab = "suggestions" }
-  }
-end)
+execute.view{ module = "issue", view = "_sidebar_state", params = {
+  initiative = initiative
+} }
 
-execute.view{
-  module = "suggestion",
-  view = "show_tab",
+execute.view { 
+  module = "issue", view = "_sidebar_issue", 
   params = {
-    suggestion = suggestion
+    issue = initiative.issue,
+    highlight_initiative_id = initiative.id
   }
 }
+
+execute.view {
+  module = "issue", view = "_sidebar_whatcanido",
+  params = { initiative = initiative }
+}
+
+execute.view { 
+  module = "issue", view = "_sidebar_members", params = {
+    issue = initiative.issue, initiative = initiative
+  }
+}
+
+
+
+execute.view {
+  module = "issue", view = "_head", params = {
+    issue = initiative.issue
+  }
+}
+
+
+ui.section( function()
+  ui.sectionHead( function()
+    ui.link{
+      module = "initiative", view = "show", id = initiative.id,
+      content = function ()
+        ui.heading { 
+          level = 1,
+          content = initiative.display_name
+        }
+      end
+    }
+    ui.heading { level = 2, content = _("Suggestion for improvement #{id}", { id = suggestion.id }) }
+  end )
+  ui.sectionRow( function()
+
+    ui.heading{ level = 2, content = suggestion.name }
+    if app.session:has_access("authors_pseudonymous") and suggestion.author then 
+      util.micro_avatar(suggestion.author)
+    end
+  end )
+  ui.sectionRow( function()
+    ui.container{
+      attr = { class = "suggestion_content wiki" },
+      content = function()
+        slot.put(suggestion:get_content("html"))
+      end
+    }
+    
+  end )
+end )
+
+ui.section( function()
+  ui.sectionHead( function()
+    ui.heading { level = 2, content = _"Collective rating" }
+  end )
+  ui.sectionRow( function()
+
+    execute.view{
+      module = "suggestion",
+      view = "_list_element",
+      params = {
+        suggestions_selector = Suggestion:new_selector():add_where{ "id = ?", suggestion.id },
+        initiative = suggestion.initiative,
+        show_name = false,
+        show_filter = false
+      }
+    }
+  end)
+end)
+  
+if app.session:has_access("all_pseudonymous") then
+  ui.section( function()
+    ui.sectionHead( function()
+      ui.heading { level = 2, content = _"Individual ratings" }
+    end )
+    ui.sectionRow( function()
+
+      execute.view{
+        module = "opinion",
+        view = "_list",
+        params = { 
+          opinions_selector = Opinion:new_selector()
+            :add_where{ "suggestion_id = ?", suggestion.id }
+            :join("member", nil, "member.id = opinion.member_id")
+            :add_order_by("member.id DESC")
+        }
+      }
+
+    end)
+  end)
+end
