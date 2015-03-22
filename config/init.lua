a7c935615559251cd0aac78ec46660c9a2373793
bsw <bsw@localhost>
2015-03-22 18:54:08 +0100

    Removed sql tracer initialization

--- config/init.lua
+++ config/init.lua
@@ -77,14 +77,6 @@ if not listen then
 
   function mondelefant.class_prototype:get_db_conn() return db end
 
-  -- enable output of SQL commands in trace system
-  function db:sql_tracer(command)
-    return function(error_info)
-      local error_info = error_info or {}
-      trace.sql{ command = command, error_position = error_info.position }
-    end
-  end
-
   -- close the database at exit
   at_exit(function() 
     db:close()
