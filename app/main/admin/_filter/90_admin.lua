f9a5dfe9337ed717d9ca41ca90180c12dd719276
bsw <bsw@localhost>
2015-03-24 22:14:44 +0100

    Fixed usage of cgi in admin filter

--- app/main/admin/_filter/90_admin.lua
+++ app/main/admin/_filter/90_admin.lua
@@ -3,7 +3,7 @@ if not app.session.member.admin then
 end
 
 if config.admin_logger then
-  config.admin_logger(cgi.params)
+  config.admin_logger(request.get_param_strings())
 end
 
 execute.inner()
