c12c92949b4e8421907a11ce53d76861fbdbd5a3
jbe <jbe@localhost>
2015-03-24 18:38:39 +0100

    Work on network configuration

--- app/main/_prefork/10_init.lua
+++ app/main/_prefork/10_init.lua
@@ -1,20 +1,36 @@
-if not config.fork then
+if config.fork == nil then
   config.fork = {}
 end
 
-if not config.fork.pre then
-  config.fork.pre = 4
+if config.fork.pre == nil then
+  config.fork.pre = 2
 end
 
-if not config.fork.max then
+if config.fork.min == nil then
+  config.fork.min = 4
+end
+
+if config.fork.max == nil then
   config.fork.max = 8
 end
 
-if not config.fork.delay then
-  config.fork.delay = 1
+if config.fork.delay == nil then
+  config.fork.delay = 0.125
 end
 
-if not config.port then
+if config.fork.error_delay == nil then
+  config.fork.error_delay = 2
+end
+
+if config.fork.exit_delay == nil then
+  config.fork.exit_delay = 2
+end
+
+if config.fork.idle_timeout == nil then
+  config.fork.idle_timeout = 900
+end
+
+if config.port == nil then
   config.port = 8080
 end
 
@@ -22,14 +38,39 @@ if config.localhost == nil then
   config.localhost = true
 end
 
+local listen_options = {
+  pre_fork              = config.fork.pre,
+  min_fork              = config.fork.min,
+  max_fork              = config.fork.max,
+  fork_delay            = config.fork.delay,
+  fork_error_delay      = config.fork.error_delay,
+  exit_delay            = config.fork.exit_delay,
+  idle_timeout          = config.fork.idle_timeout,
+  min_requests_per_fork = config.fork.min_requests,
+  max_requests_per_fork = config.fork.max_requests,
+  http_options          = config.http_options
+}
+
+if config.ipv6 then
+  listen_options[#listen_options+1] = { proto = "tcp6", port = config.port, localhost = config.localhost }
+end
+if config.ipv6 ~= "only" then
+  listen_options[#listen_options+1] = { proto = "tcp4", port = config.port, localhost = config.localhost }
+end
+
+listen(listen_options)
+
 listen{
- { proto = "tcp4", port = config.port, localhost = true },
- { proto = "interval", delay = 5, handler = function() 
-    Event:send_pending_notifications()
- end },
- pre_fork = config.fork.pre,
- max_fork = config.fork.max,
- fork_delay = config.fork.delay
+  {
+    proto = "interval",
+    name  = "send_pending_notifications",
+    delay = 5,
+    handler = function() 
+      Event:send_pending_notifications()
+    end
+  },
+  min_fork = 1,
+  max_fork = 1
 }
 
 execute.inner()
