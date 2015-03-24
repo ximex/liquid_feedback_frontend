2a9061ab0cd11b05bab12155da421294fee54853
bsw <bsw@localhost>
2015-03-24 11:50:08 +0100

    Updated event notifications and moved code from config to prefork initializer

--- config/init.lua
+++ config/init.lua
@@ -108,32 +108,4 @@ if not listen then
     request.add_header("Cache-Control", "max-age=3600");
   end
 
-  return
 end
-
-if not config.fork then
-  config.fork = {}
-end
-
-if not config.fork.pre then
-  config.fork.pre = 4
-end
-
-if not config.fork.max then
-  config.fork.max = 8
-end
-
-if not config.fork.delay then
-  config.fork.delay = 1
-end
-
-if not config.port then
-  config.port = 8080
-end
-
-listen{
-  { proto = "tcp4", port = config.port, localhost = true },
-  pre_fork = config.fork.pre,
-  max_fork = config.fork.max,
-  fork_delay = config.fork.delay
-}
