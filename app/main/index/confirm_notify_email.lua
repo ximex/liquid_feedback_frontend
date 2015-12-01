96695e19a6947023cb38a9a2dd47b856fffbbcb5
bsw <bsw@localhost>
2015-12-01 18:33:06 +0100

    Fixed layout of email address confirmation dialog

--- app/main/index/confirm_notify_email.lua
+++ app/main/index/confirm_notify_email.lua
@@ -1,22 +1,29 @@
-slot.put_into("title", _"Email address confirmation")
+ui.title(_"Email address confirmation")
 
-ui.form{
-  attr = { class = "vertical" },
-  module = "index",
-  action = "confirm_notify_email",
-  routing = {
-    ok = {
-      mode = "redirect",
+ui.section(function() 
+
+  ui.sectionHead(function()
+
+    ui.form{
+      attr = { class = "vertical" },
       module = "index",
-      view = "index"
+      action = "confirm_notify_email",
+      routing = {
+        ok = {
+          mode = "redirect",
+          module = "index",
+          view = "index"
+        }
+      },
+      content = function()
+        ui.field.text{
+          label = _"Confirmation code",
+          name = "secret",
+          value = param.get("secret")
+        }
+        ui.submit{ text = _"Confirm" }
+      end
     }
-  },
-  content = function()
-    ui.field.text{
-      label = _"Confirmation code",
-      name = "secret",
-      value = param.get("secret")
-    }
-    ui.submit{ text = _"Confirm" }
-  end
-}
+
+  end)
+end)
