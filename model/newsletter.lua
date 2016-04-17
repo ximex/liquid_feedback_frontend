4653fdc0a69eb78b1ff23e1d46bb27d4737553bf
bsw <bsw@localhost>
2016-04-17 16:02:26 +0200

    Minor fixes to new notification system

--- model/newsletter.lua
+++ model/newsletter.lua
@@ -6,36 +6,37 @@ function Newsletter:send_next_newsletter()
   if not newsletter_to_send then
     return false
   end
+
   local newsletter = newsletter_to_send.newsletter
   
   local newsletter_to_send = NewsletterToSend:by_newsletter_id(newsletter.id)
   newsletter_to_send:load("member")
-  
+
   newsletter.sent = "now"
   newsletter:save()
-
+  
   io.stderr:write("Sending newsletter " .. newsletter.id .. " to " .. (#newsletter_to_send) .. " members\n")
   
   for i, n in ipairs(newsletter_to_send) do
     
     local member = newsletter_to_send.member
     
-    if not member.notify_email then
-      return
+    if member.notify_email then
+      io.stderr:write("Sending newsletter " .. newsletter.id .. " to " .. member.notify_email .. "\n")
+      
+      local success = net.send_mail{
+        envelope_from = config.mail_envelope_from,
+        from          = config.mail_from,
+        reply_to      = config.mail_reply_to,
+        to            = member.notify_email,
+        subject       = newsletter.subject,
+        content_type  = "text/plain; charset=UTF-8",
+        content       = newsletter.content
+      }
     end
 
-    io.stderr:write("Sending newsletter " .. newsletter.id .. " to " .. member.notify_email .. "\n")
-    
-    local success = net.send_mail{
-      envelope_from = config.mail_envelope_from,
-      from          = config.mail_from,
-      reply_to      = config.mail_reply_to,
-      to            = member.notify_email,
-      subject       = newsletter.subject,
-      content_type  = "text/plain; charset=UTF-8",
-      content       = newsletter.content
-    }
-
   end
+  
+  return true
   
 end
--- model/scheduled_notification_to_send.lua
+++ model/scheduled_notification_to_send.lua
@@ -4,6 +4,7 @@ ScheduledNotificationToSend.table = 'scheduled_notification_to_send'
 function ScheduledNotificationToSend:get_next()
   return self:new_selector()
     :add_order_by("pending DESC")
+    :add_order_by("random()")
     :limit(1)
     :optional_object_mode()
     :exec()
