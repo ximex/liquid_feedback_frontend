21089d9dd69322ee1412a29af60ceecd29c710d1
bsw <bsw@localhost>
2016-04-17 17:26:55 +0200

    Fixes on notification system

--- app/main/member/_action/update_notify_level.lua
+++ app/main/member/_action/update_notify_level.lua
@@ -5,17 +5,22 @@ if app.session.member.disable_notifications ~= disable_notifications then
   app.session.member.disable_notifications = disable_notifications
 end
 
-if param.get("digest") == "true" then
-  local dow = param.get("notification_dow")
-  if dow == "daily" then
-    app.session.member.notification_dow = nil
-  else
-    app.session.member.notification_dow = tonumber(dow)
-  end
-  app.session.member.notification_hour = tonumber(param.get("notification_hour"))
-else
+if app.session.member.disable_notifications then
   app.session.member.notification_dow = nil
   app.session.member.notification_hour = nil
+else 
+  if param.get("digest") == "true" then
+    local dow = param.get("notification_dow")
+    if dow == "daily" then
+      app.session.member.notification_dow = nil
+    else
+      app.session.member.notification_dow = tonumber(dow)
+    end
+    app.session.member.notification_hour = tonumber(param.get("notification_hour"))
+  else
+    app.session.member.notification_dow = nil
+    app.session.member.notification_hour = nil
+  end
 end
 
 app.session.member:save()
